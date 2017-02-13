3913bff3b358efa48c5d27be91f8834b1868d32f
ezat123 <ezat.bscs@gmail.com>
2017-02-13 16:44:49 +0500

    var port used

--- server.js
+++ server.js
@@ -5,13 +5,13 @@ app.get('/', function (req, res, next) {
   res.send('Hello World!')
 })
 
-var prot = (process.env.PORT || 3000)
+var port = (process.env.PORT || 3000)
 
 app.get('/home', function(req,res,next){
     res.send('this. is Home')
 })
 
 
-app.listen(4000, function () {
-  console.log('Example app listening on port 4000!')
+app.listen(port, function () {
+  console.log('Example app listening on port !')
 })
